71dd06611f539e008cb782ca18b73723087ed859
Yurii Pochapskiy <pochapskiy1983@gmail.com>
2025-01-08 23:41:13 +0200

    add project

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'header-blue': '#585982',
+        'text-primary-red': '#F0145A',
+        'main-blue': '#313264',
+        'main-h2-color': '#24A3FF',
+        'first-underline': '#CACADB',
+        'second-underline': '#E7E7F2',
+        'main-h3': '#323264',
+        'hillel-bg': '#1C4280',
+        'wanna-biz': '#143646',
+        'tapgerine': '#F48B30',
+        'three-d-look': '#9E4DC3',
+        'orangear': '#FD9339',
+        'adtelligent': '#E2E2E4',
+        'footer-bg': '#F5F5FA',
+        'color-tel': '#46466E',
+        'copyright': '#B3B3BA',
+      },
+    },
+  },
+  plugins: [
+
+  ],
+};
+
